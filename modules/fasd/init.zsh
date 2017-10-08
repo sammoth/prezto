762b307f61c805afac84f252df93a4ce5016026c
sammoth <sammoth.th@gmail.com>
2017-10-08 11:44:40 +0100

    tweak fasd

--- modules/fasd/init.zsh
+++ modules/fasd/init.zsh
@@ -36,18 +36,18 @@ source "$cache_file"
 
 unset cache_file init_args
 
-fasd_cd() {
-  local fasd_ret="$(fasd -d "$@")"
-  if [[ -d "$fasd_ret" ]]; then
-    cd "$fasd_ret"
-  else
-    print "$fasd_ret"
-  fi
-}
+#fasd_cd() {
+#  local fasd_ret="$(fasd -d "$@")"
+#  if [[ -d "$fasd_ret" ]]; then
+#    cd "$fasd_ret"
+#  else
+#    print "$fasd_ret"
+#  fi
+#}
 
 #
 # Aliases
 #
 
 # Changes the current working directory interactively.
-alias j='fasd_cd -i'
+#alias j='fasd_cd -i'
